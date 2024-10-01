7029d004dc71566093dddb677c84e3bbb7f7acf2
ajay <ajay@localhost.localdomain>
2024-10-01 19:47:35 +0530

    file updated

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
     const token = req.cookies.token;
+    console.log(token);
     if (!token) {
         return res.status(401).json({ message: 'Unauthorized' });
     }
