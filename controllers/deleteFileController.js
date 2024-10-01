c17a113022f87ecfc98da2dfb2bc04e73eb57d76
ajay <ajay@localhost.localdomain>
2024-10-01 20:02:14 +0530

    file updated

--- controllers/deleteFileController.js
+++ controllers/deleteFileController.js
@@ -1,9 +1,34 @@
 const fileModel = require("../models/fileModel");
+const cloudinary = require("../config/cloudinaryConfig");
 
 const deleteFileController =  async (req, res) => {
     try {
         const fileId = req.params.id;
-        await fileModel.findByIdAndDelete(fileId); // Delete the file by ID
+
+        const file = await fileModel.findByIdAndDelete(fileId); // Delete the file by ID
+        if (!file) {
+            return res.status(404).json({ message: "File not found" });
+        }
+
+        // Import cloudinary configuration
+
+        // Loop through the fileUrl array and destroy each file from Cloudinary
+        for (const url of file.fileUrl) {
+            console.log("url ",url);
+            const publicId = url.split('/').pop().split('.')[0];
+            console.log("publicId ",publicId);
+            await new Promise((resolve, reject) => {
+                cloudinary.uploader.destroy(publicId, (error, result) => {
+                    if (error) {
+                        console.error(`Error deleting file from Cloudinary: ${error}`);
+                        reject(error);
+                    } else {
+                        console.log(`File deleted from Cloudinary: `,result);
+                        resolve(result);
+                    }
+                });
+            });
+        }
         res.status(200).json({ message: "File deleted successfully" });
     } catch (error) {
         res.status(500).json({ message: "Error deleting file" });
